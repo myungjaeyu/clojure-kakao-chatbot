15ad32a8c2315721e508ee6702641de5a81aa860
u4bi <admin@naver.id>
2018-05-09 21:57:52 +0900

    Added dependencies.

--- project.clj
+++ project.clj
@@ -1,6 +1,10 @@
 (defproject kakao_chatbot "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]])
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [ring/ring-core "1.6.3"]
+                 [ring/ring-jetty-adapter "1.6.3"]
+                 [compojure "1.6.1"]
+                 [ring/ring-json "0.4.0"]
+                 [ring-cors "0.1.12"]]
+  :plugins [[lein-ring "0.12.4"]]
+  :ring {:handler kakao-chatbot.core/app})
