e1c57ecec466ef4dff4652727f228881f5902668
u4bi <admin@naver.id>
2018-05-09 21:35:32 +0900

    Initial

---
+++ src/kakao_chatbot/core.clj
@@ -0,0 +1,6 @@
+(ns kakao-chatbot.core)
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
---
+++ test/kakao_chatbot/core_test.clj
@@ -0,0 +1,7 @@
+(ns kakao-chatbot.core-test
+  (:require [clojure.test :refer :all]
+            [kakao-chatbot.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
