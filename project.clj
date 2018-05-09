18afc45eedd2ecdcf213ff4a660ec5c291ab9e27
u4bi <admin@naver.id>
2018-05-10 00:47:25 +0900

    Added compojure handler.

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
 (defproject kakao_chatbot "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.clojure/core.match "0.3.0-alpha5"]
                  [ring/ring-core "1.6.3"]
                  [ring/ring-jetty-adapter "1.6.3"]
                  [compojure "1.6.1"]
